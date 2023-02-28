ae5c41683dd8c979ba517dc5a7da50f2ae016184
Joel king yan Hii <jhii@u91z05s05.42kl.edu.my>
2023-02-28 18:19:45 +0800

    joel - removed 'webserv from .gitignore' because webserve.hpp file is being ignores && added color definition in webserv.hpp

---
+++ Webserv/incs/webserv.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 18:13:39 by schuah            #+#    #+#             */
+/*   Updated: 2023/02/28 18:16:54 by jhii             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+# define RESET		"\033[0m"
+# define BLACK		"\033[30m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+# define YELLOW		"\033[33m"
+# define BLUE		"\033[34m"
+# define MAGENTA	"\033[35m"
+# define CYAN		"\033[36m"
+# define WHITE		"\033[37m"
+
+# include <iostream>
+
+#endif
