85093f4f11fda89784154d6cd9b75d8b519196ec
Joel king yan Hii <jhii@u91z05s01.42kl.edu.my>
2023-03-10 17:04:04 +0800

    initialize euleewallet

--- Webserv/incs/EuleeWallet.hpp
+++ Webserv/incs/EuleeWallet.hpp
@@ -6,7 +6,7 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:10:48 by jhii              #+#    #+#             */
-/*   Updated: 2023/03/10 16:18:13 by jhii             ###   ########.fr       */
+/*   Updated: 2023/03/10 17:00:11 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ enum	RegularExpression
 struct EuleeWallet
 {
 	EuleeWallet(void);
+	EuleeWallet(RegularExpression priority, std::string valuePath, std::string root, std::string index, std::string include, std::string cgiPass, std::string cgiIndex, std::string cgiParam);
 	~EuleeWallet(void);
 
 	RegularExpression	priority;
--- Webserv/srcs/EuleeWallet.cpp
+++ Webserv/srcs/EuleeWallet.cpp
@@ -6,12 +6,24 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 15:12:18 by jhii              #+#    #+#             */
-/*   Updated: 2023/03/10 14:45:16 by jhii             ###   ########.fr       */
+/*   Updated: 2023/03/10 17:02:33 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EuleeWallet.hpp"
 
-EuleeWallet::EuleeWallet(void) {}
+EuleeWallet::EuleeWallet(void) : priority(), valuePath(), root(), index(), include(), cgiPass(), cgiIndex(), cgiParam() {}
+
+EuleeWallet::EuleeWallet(RegularExpression priority, std::string valuePath, std::string root, std::string index, std::string include, std::string cgiPass, std::string cgiIndex, std::string cgiParam) : priority(), valuePath(), root(), index(), include(), cgiPass(), cgiIndex(), cgiParam()
+{
+	this->priority = priority;
+	this->valuePath = valuePath;
+	this->root = root;
+	this->index = index;
+	this->include = include;
+	this->cgiPass = cgiPass;
+	this->cgiIndex = cgiIndex;
+	this->cgiParam = cgiParam;
+}
 
 EuleeWallet::~EuleeWallet(void) {}
